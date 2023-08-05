3293f6515731338e85522fbcf0d91d2c1c2078f6
priyansh03sharma <132761882+priyansh03sharma@users.noreply.github.com>
2023-08-05 20:49:45 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,1 +1,35 @@
-# OIBSIP
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int minRange = 1;
+        int maxRange = 100;
+        int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have chosen a number between " + minRange + " and " + maxRange + ".");
+        System.out.println("Can you guess what it is?");
+
+        while (true) {
+            System.out.print("Enter your guess: ");
+            int guess = scanner.nextInt();
+            attempts++;
+
+            if (guess == randomNumber) {
+                System.out.println("Congratulations! You guessed the correct number (" + randomNumber + ") in " + attempts + " attempts.");
+                break;
+            } else if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("Too high! Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+}
